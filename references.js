78cc5f5ec9e0aa252c15a39c85366ac1dac928eb
Chris Harvey <chrisharvey2pi@gmail.com>
2015-03-05 01:38:10 -0500

    fixed issues with multiple authors and publisher name ending in '.'

--- references.js
+++ references.js
@@ -43,11 +43,30 @@ function addReferencesToList(reference_array, html_list) {
         .attr('itemscope','').attr('itemtype', function (d) { return d.itemtype; })
         .attr('id', function (d) { return d.id; });
 
-  cite.author = cite.append('span').attr('itemprop','author') // array
-    .append('span') // array element
-      .attr('itemscope','').attr('itemtype','https://schema.org/Person')
-      .append('span').attr('itemprop','name')
-        .text(function (d) { return d.author[0].name; });
+  cite.author = cite.append('span').attr('itemprop','author');
+
+    cite.author.person = cite.author.selectAll('span[itemtype="https://schema.org/Person"]')
+      .data(function (d) { return d.author; }).enter()
+      .append('span')
+        .attr('itemscope','').attr('itemtype','https://schema.org/Person')
+        .text(function (e, i) {
+          (function count(property) {
+            // provides a count on the number of items in the `property` array of a datum
+            for (var i in reference_array) {
+              for (var j in reference_array[i][property]) {
+                reference_array[i][property][j].parent_length = reference_array[i][property].length;
+              }
+            }
+          })('author');
+          var returned = '';
+          if (i > 0) { returned += ',';
+            if (i === e.parent_length-1) returned += ' & ';
+          }
+          return returned;
+        });
+
+      cite.author.person.name = cite.author.person.append('span').attr('itemprop','name')
+        .text(function (e) { return e.name; });
 
   cite.datePublished = cite.append('time').attr('itemprop','datePublished')
     .attr('datetime', function (d) { return d.datePublished; })
@@ -69,7 +88,7 @@ function addReferencesToList(reference_array, html_list) {
           .text(function (d) { return ' ' + d.publisher.location.address.addressLocality + ','; })
 
         cite.publisher.location.address.append('span').attr('itemprop','addressRegion')
-          .text(function (d) { return ' ' + d.publisher.location.address.addressRegion + ','; });
+          .text(function (d) { return ' ' + d.publisher.location.address.addressRegion + ':'; });
 
       cite.publisher.location.geo = cite.publisher.location.append('span').attr('itemprop','geo')
         .attr('itemscope','').attr('itemtype','https://schema.org/GeoCoordinates');
@@ -81,7 +100,9 @@ function addReferencesToList(reference_array, html_list) {
           .attr('content', function (d) { return d.publisher.location.geo.longitude; });
 
     cite.publisher.name = cite.publisher.append('span').attr('itemprop','name')
-      .text(function (d) { return ' ' + d.publisher.name + '.' });
+      .text(function (d) {
+        return ' ' + d.publisher.name + (d.publisher.name.substr(-1) === '.' ? '' : '.')
+      });
 
   // for (var j in reference_array) {
   //   var thisref = reference_array[j];
@@ -121,4 +142,54 @@ function addReferencesToList(reference_array, html_list) {
   //     )
   //   ));
   // }
+
+  /**
+    * Appends an HTML element, marked up with microdata, to a given element. This function is
+    * cleaner, but more verbose. Examples below. The first is using normal d3 syntax while
+    * the second uses this function.
+  ```
+  var user_rating = user_review.append('span').attr('itemprop', 'rating')
+    .attr('itemscope','').attr('itemtype','https://schema.org/Rating')
+    .text(function (d) { return d.rating + ' out of 5 stars.'; });
+  user_rating.append('meta').attr('itemprop','ratingValue')
+    .attr('content',function (d) { return d.rating; });
+  user_rating.append('meta').attr('itemprop','bestRating')
+    .attr('content', 5);
+  ```
+  ```
+  var user_rating = appendMicrodata(user_review, {
+    element: 'span',
+    itemprop: 'rating',
+    itemtype: 'https://schema.org/Rating',
+    text: function (d) { return d.rating + ' out of 5 stars.'; }
+  });
+  appendMicrodata(user_rating, {
+    element: 'meta',
+    itemprop: 'ratingValue',
+    content: function (d) { return d.rating; }
+  });
+  appendMicrodata(user_rating, {
+    element: 'meta',
+    itemprop: 'bestRating',
+    content: 5
+  });
+  ```
+  @param d3object the object on which to call d3's `.append()` method
+  @param args     an object of the form:
+  {
+    element  : required string. the name of the element to append (with no angle brackets),
+    itemprop : optional string. the value(s) of the `itemprop` attribute of the new element,
+    itemtype : optional string. the full value of the `itemtype` attribute,
+    content  : optional string or function. takes (d) as an argument and returns the `[content]` attribute's value,
+    text     : optional string or function. takes (d) as an argument and returns the text,
+  }
+    */
+  function appendMicrodata(d3object, args) {
+    var el = d3object.append(args.element);
+    if (args.itemprop) el.attr('itemprop',args.itemprop);
+    if (args.itemtype) el.attr('itemscope','').attr('itemtype',args.itemtype);
+    if (args.content)  el.attr('content',args.content);
+    el.text(args.text || '');
+    return el;
+  }
 }
